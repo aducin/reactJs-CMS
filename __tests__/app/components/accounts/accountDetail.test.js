985ab667a9d06f3951c8099c492f2ac00b2e497c
aducin <albert.ducin@gmail.com>
2018-08-10 22:36:11 +0200

    some new tests

--- __tests__/app/components/accounts/accountDetail.test.js
+++ __tests__/app/components/accounts/accountDetail.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 
+import { configure, mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import Config from '../../../../src/app/Config';
 import AccountDetail from '../../../../src/app/components/account/AccountDetail.jsx';
 import state from '../../../../src/app/helper/accountState';
+
+configure({ adapter: new Adapter() });
 
 const ascending = true;
 const data = {
@@ -41,7 +46,7 @@ const mockFunction = () => { return false; };
 const selectedRow = 1;
 const sortBy = 'id';
 
-const details = TestRenderer.create(
+const component = (
   <AccountDetail
     ascending={ascending}
     data={data}
@@ -52,8 +57,24 @@ const details = TestRenderer.create(
     sortBy={sortBy}
   />
 );
+const details = TestRenderer.create(component);
 
-it('AccountDetails renders correctly', async () => {
-  const tree = details.toJSON();
-  expect(tree).toMatchSnapshot();
+describe('AccountHeader snapshot', () => {
+  it('AccountDetails renders correctly', async () => {
+    const tree = details.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('AccountDetails tests', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(component);
+  });
+  it('there are 4 table rows', () => {
+    expect(wrapper.find('tr')).toHaveLength(4);
+  });
+  it('last row in the second column contains the following values', () => {
+    expect(wrapper.find('tr').last().childAt(1).props().children).toEqual(["10.99", "zł"]);
+  });
 });
